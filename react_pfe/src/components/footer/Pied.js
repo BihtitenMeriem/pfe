37591ed720a51a3cecd32fe076e573b7d7778234
Marybih <marybih41@gmail.com>
2023-05-23 16:02:27 +0100

    v3

--- react_pfe/src/components/footer/Pied.js
+++ react_pfe/src/components/footer/Pied.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react'
-
+import './style.css'
 export default function Pied() {
   return (
     <div>
---
+++ react_pfe/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+export default function Header() {
+  return (
+        <div className="header">
+         {/* <div className='bib'> */}
+            <img src="img/header/logo.png" alt="" />
+            <h1 className='title'>BIB_BOOK</h1>
+         {/* </div> */}
+            
+            <button>Connection</button>
+        </div>
+
+    
+  )
+}
